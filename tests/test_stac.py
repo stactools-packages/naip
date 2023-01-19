b2fd7c7cbeecac5b40380dad812f93618b1c92dc
Neeraj Sirdeshmukh <sirneeraj@gmail.com>
2023-01-19 10:41:02 -0600

    updated tests

--- tests/test_stac.py
+++ tests/test_stac.py
@@ -1,6 +1,7 @@
 import unittest
 
-from stactools.naip.stac import create_collection
+from stactools.naip.stac import create_collection, create_item
+from tests import test_data
 
 
 class StacTest(unittest.TestCase):
@@ -9,3 +10,13 @@ class StacTest(unittest.TestCase):
 
         collection.set_self_href("http://example.com/collection.json")
         collection.validate()
+
+    def test_create_item(self):
+        item = create_item(
+            "al",
+            "2011",
+            test_data.get_path("data-files/m_3008501_ne_16_1_20110815-downsampled.tif"),
+            test_data.get_path("data-files/m_3008501_ne_16_1_20110815.txt"),
+        )
+
+        self.assertEqual(item.properties["sci:doi"], "10.5066/F7QN651G")

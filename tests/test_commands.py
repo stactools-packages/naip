c9276dca9a63e617f5e808c1098a91bd73ee07cf
Neeraj Sirdeshmukh <sirneeraj@gmail.com>
2023-01-19 14:55:10 -0600

    Add scientific extension (#21)
    * Add scientific extension
    
    * ci: update for no-binary rasterio, 3.10
    
    * setup: support 3.10 in setup.cfg
    
    * ci: pin gdal in conda env
    
    * updated tests
    
    * move tests for raster extension into test_stac.py
    
    * update tests
    
    * fix: don't add scientific extension in test
    
    Co-authored-by: Pete Gadomski <pete.gadomski@gmail.com>

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -2,7 +2,6 @@ import os
 from tempfile import TemporaryDirectory
 
 import pystac
-from pystac.extensions.raster import DataType, RasterExtension
 from stactools.testing import CliTestCase
 
 from stactools.naip.commands import create_naip_command
@@ -40,15 +39,6 @@ class CreateItemTest(CliTestCase):
             item = pystac.Item.from_file(item_path)
 
         item.validate()
-
-        image_asset = item.assets["image"]
-        raster_ext = RasterExtension.ext(image_asset)
-        self.assertEqual(len(raster_ext.bands), 4)
-        for raster_band in RasterExtension.ext(image_asset).bands:
-            self.assertEqual(raster_band.nodata, 0)
-            self.assertEqual(raster_band.spatial_resolution, item.properties["gsd"])
-            self.assertEqual(raster_band.data_type, DataType.UINT8)
-            self.assertEqual(raster_band.unit, "none")
 
     def test_create_item_no_resource_desc(self):
         fgdc_href = test_data.get_path("data-files/m_4207201_ne_18_h_20160809.txt")
--- tests/test_stac.py
+++ tests/test_stac.py
@@ -1,6 +1,10 @@
 import unittest
 
-from stactools.naip.stac import create_collection
+from pystac.extensions.raster import DataType, RasterExtension
+from pystac.extensions.scientific import ItemScientificExtension
+
+from stactools.naip.stac import create_collection, create_item
+from tests import test_data
 
 
 class StacTest(unittest.TestCase):
@@ -9,3 +13,25 @@ class StacTest(unittest.TestCase):
 
         collection.set_self_href("http://example.com/collection.json")
         collection.validate()
+
+    def test_create_item(self):
+        item = create_item(
+            "al",
+            "2011",
+            test_data.get_path("data-files/m_3008501_ne_16_1_20110815-downsampled.tif"),
+            test_data.get_path("data-files/m_3008501_ne_16_1_20110815.txt"),
+        )
+
+        image_asset = item.assets["image"]
+        raster_ext = RasterExtension.ext(image_asset)
+        self.assertEqual(len(raster_ext.bands), 4)
+        for raster_band in RasterExtension.ext(image_asset).bands:
+            self.assertEqual(raster_band.nodata, 0)
+            self.assertEqual(raster_band.spatial_resolution, item.properties["gsd"])
+            self.assertEqual(raster_band.data_type, DataType.UINT8)
+            self.assertEqual(raster_band.unit, "none")
+
+        sci_ext = ItemScientificExtension.ext(item)
+
+        self.assertEqual(sci_ext.doi, "10.5066/F7QN651G")
+        self.assertGreaterEqual(len(sci_ext.publications), 1)

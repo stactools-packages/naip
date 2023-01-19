05f972ac807933a3d40b0b13a4fa489ff6576418
Neeraj Sirdeshmukh <sirneeraj@gmail.com>
2023-01-19 11:23:08 -0600

    move tests for raster extension into test_stac.py

--- tests/test_stac.py
+++ tests/test_stac.py
@@ -1,5 +1,7 @@
 import unittest
 
+from pystac.extensions.raster import DataType
+
 from stactools.naip.stac import create_collection, create_item
 from tests import test_data
 
@@ -18,5 +20,14 @@ class StacTest(unittest.TestCase):
             test_data.get_path("data-files/m_3008501_ne_16_1_20110815-downsampled.tif"),
             test_data.get_path("data-files/m_3008501_ne_16_1_20110815.txt"),
         )
+
+        image_asset = item.assets["image"].to_dict()
+        raster_bands = image_asset["raster:bands"]
+        self.assertEqual(len(raster_bands), 4)
+        for raster_band in raster_bands:
+            self.assertEqual(raster_band["nodata"], 0)
+            self.assertEqual(raster_band["spatial_resolution"], item.properties["gsd"])
+            self.assertEqual(raster_band["data_type"], DataType.UINT8)
+            self.assertEqual(raster_band["unit"], "none")
 
         self.assertEqual(item.properties["sci:doi"], "10.5066/F7QN651G")
